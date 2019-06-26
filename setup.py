e13feacbe8e07916078d433fde0bb088d28f5eac
Adina Feinstein <adina.d.feinstein@gmail.com>
2019-06-26 16:13:47 -0500

    created setup.py file

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+
+import os
+import sys
+from setuptools import setup
+
+sys.path.insert(0, "eleanor")
+from version import __version__
+
+
+long_description = \
+    """
+sally is a python package to characterize flares for stars in the 
+TESS Full Frame Images.
+"""
+
+
+setup(
+    name='sally',
+    version=__version__,
+    license='MIT',
+    author='Adina D. Feinstein',
+    author_email='adina.d.feinstein@gmail.com',
+    packages=[
+        'sally',
+        ],
+    include_package_data=True,
+    url='http://github.com/afeinstein20/sally',
+    description='For characterizing flares in the TESS FFIs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_data={'': ['README.md', 'LICENSE']},
+    install_requires=[
+        'tqdm', 'lightkurve', 'astropy',
+        'astroquery', 'exoplanet', 'altaipony',
+        'setuptools>=41.0.0',
+        'tensorflow', 'vaneska', 'beautifulsoup4>=4.6.0', 'tess-point'],
+
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.0',
+        ],
+    )
