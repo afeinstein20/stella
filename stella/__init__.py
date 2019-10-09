9b8fb87358826e28be6b60c1ea34da4b55fcc456
Adina Feinstein <adina.d.feinstein@gmail.com>
2019-10-09 08:41:17 -0500

    juunnkk

--- stella/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
-
-from .version import __version__
-from .neural_network   import *
-from .simulate         import *
-from .characterization import *
--- stella/tests/test_neural_network.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-
-def test_anything():
-    assert(1==1)
