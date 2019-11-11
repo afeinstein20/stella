9db27715191d17f2cd1b6806b56abc99af5f2819
Adina Feinstein <adina.d.feinstein@gmail.com>
2019-11-11 10:13:17 -0600

    no more simulated lc, real bois only

--- stella/__init__.py
+++ stella/__init__.py
@@ -3,5 +3,5 @@ PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 
 from .version import __version__
 from .neural_network   import *
-from .simulate         import *
 from .characterization import *
+from .data_shuffle     import *
