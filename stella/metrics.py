022c16b6bc29de48a4375cacc93dd30082c22905
Adina Feinstein <adina.d.feinstein@gmail.com>
2020-02-08 06:49:58 +1100

    updated metrics

--- stella/metrics.py
+++ stella/metrics.py
@@ -1,5 +1,6 @@
 import os, sys
 import numpy as np
+from pylab import *
 from astropy.table import Table, Column
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
@@ -19,7 +20,6 @@ class ModelMetrics(object):
         """
         Initializes class. Requires a directory where all
         of the files from the same model runs are saved
-
         Parameters
         ----------
         fn_dir : str
@@ -42,7 +42,6 @@ class ModelMetrics(object):
         """
         Pareses file names in fn_dir for the seed, number of epochs, 
         fractional balance, and fold (for cross validation).
-
         Attributes
         ----------
         seed : int, np.array
@@ -104,7 +103,7 @@ class ModelMetrics(object):
         for cn in colnames:
             mean_arr.append(np.round(self.predval_table[cn].data, 3))
         self.predval_table.add_column(Column(np.nanmean(mean_arr, axis=0),
-                                             name='mean_pred'))
+                                             name='pred_mean'))
 
         if self.predtest_table is not None:
             mean_arr = []
@@ -112,7 +111,16 @@ class ModelMetrics(object):
             for cn in colnames:
                 mean_arr.append(np.round(self.predtest_table[cn].data, 3))
             self.predtest_table.add_column(Column(np.nanmean(mean_arr, axis=0),
-                                                  name='mean_pred'))
+                                                  name='pred_mean'))
+
+
+    def pred_round(self, table, threshold):
+        """ Rounds the average prediction based on a threshold. """
+        pr = np.zeros(len(table))
+        pr[table['pred_mean'].data >= threshold] = 1
+        pr[table['pred_mean'].data <  threshold] = 0
+        table.add_column(Column(pr, name='round_pred'))
+        return table
 
 
     def calculate_metrics(self, threshold=0.5, data_set='validation'):
@@ -129,7 +137,6 @@ class ModelMetrics(object):
              Sets which data set to look at. Default is 'validation'.
              Other option is 'test'. DO NOT LOOK AT THE TEST SET UNTIL
              YOU ARE COMPLETELY HAPPY WITH YOUR MODEL.
-
         Attributes
         ----------
         average_precision : float
@@ -147,8 +154,6 @@ class ModelMetrics(object):
             else:
                 raise ValueError("No test set predictions found.")
 
-        pred_round = np.zeros(len(table))
-
         # SETS ARRS FOR AVG PRECISION, ACC., RECALL SCORE, PREC. SCORE
         ap, ac, rs, ps = [], [], [], []
         p_cur, r_cur = [], []
@@ -156,28 +161,29 @@ class ModelMetrics(object):
         # SETS KEYS TO LOOK FOR IN TABLE FOR EITHER METHOD
         if self.mode is 'ensemble':
             gt  = table['gt'].data
-            key = 'pred_s'
-            pred_round[table['mean_pred'].data >= threshold] = 1
-            pred_round[table['mean_pred'].data <  threshold] = 0
+            key = 'pred_'
+            table = self.pred_round(table, threshold)
+            self.predval_table = table
 
         elif self.mode is 'cross_val':
             gt  = None
             key = 'pred_f'
 
         for i, val in enumerate([i for i in table.colnames if key in i]):
+
             if self.mode is 'cross_val':
                 gt_key = 'gt_' + val.split('_')[1]
                 gt = table[gt_key].data
 
             # CALCULATES AVERAGE PRECISION SCORE
             ap.append( np.round( average_precision_score(gt,
-                                                         table[val],
+                                                         table[val].data,
                                                          average=None), 4))
             # ROUNDED BASED ON THRESHOLD
             arr = np.copy(table[val].data)
             arr[arr >= threshold] = 1.0
             arr[arr <  threshold] = 0.0
-
+                
             # CALCULATES ACCURACY
             ac.append( np.round( np.sum(arr == gt) / len(table), 4))
 
@@ -194,9 +200,9 @@ class ModelMetrics(object):
                 r_cur.append(rec_curve)
 
         if self.mode is 'ensemble':
-            rs = np.round( recall_score( gt, pred_round), 4)
-            ps = np.round( precision_score( gt, pred_round), 4)
-            p_cur, r_cur, _ = precision_recall_curve(gt, table['mean_pred'].data)
+            rs = np.round( recall_score( gt, table['round_pred']), 4)
+            ps = np.round( precision_score( gt, table['round_pred']), 4)
+            p_cur, r_cur, _ = precision_recall_curve(gt, table['pred_mean'].data)
 
             self.average_precision = ap[-1]
             self.accuracy = ac[-1]
@@ -208,3 +214,98 @@ class ModelMetrics(object):
         self.recall_score = rs
         self.precision_score = ps
         self.prec_recall_curve = np.array([r_cur, p_cur])
+
+    
+    def confusion_matrix(self, ds, threshold=0.5, colormap='inferno', 
+                         data_set='validation'):
+        """
+        Plots the confusion matrix of true positives,
+        true negatives, false positives, and false
+        negatives.
+        Parameters
+        ----------
+        ds : stella.DataSet
+             Object needed to look at light curves from the validation
+             or the test set.
+        threshold : float, optional
+             Defines the threshold for positive vs. negative cases.
+             Default is 0.5 (50%).
+        colormap : str, optional
+             Colormap to draw colors from to plot the light curves
+             on the confusion matrix. Default is 'inferno'.
+         data_set : str, optional
+             Sets which data set to look at. Default is 'validation'.
+             Other option is 'test'. DO NOT LOOK AT THE TEST SET UNTIL
+             YOU ARE COMPLETELY HAPPY WITH YOUR MODEL. 
+        """
+        # GETS THE COLORS FOR PLOTTING
+        cmap = cm.get_cmap(colormap, 15)
+        colors = []
+        for i in range(cmap.N):
+            rgb = cmap(i)[:3]
+            colors.append(matplotlib.colors.rgb2hex(rgb))
+        colors = np.array(colors)
+
+        # PLOTTING NORMALIZED LIGHT CURVE TO GIVEN SUBPLOT
+        def plot_lc(data, ind, ax, color, offset):
+            """ Plots the light curve on a given axis. """
+            ax.set_xlim(0,200)
+            ax.set_ylim(-3,3.5)
+            ax.axvline(100, linestyle='dotted', color='gray',
+                       linewidth=0.5)
+            ax.set_yticks([])
+            ax.set_xticks([])
+
+            # NORMALIZING FLUX TO PEAK
+            lc = data[ind] - np.nanmedian(data[ind])
+            lc /= np.abs(np.nanmax(lc, axis=0))
+            lc += offset
+
+            ax.plot(lc, color=color, linewidth=2.5)
+            return ax
+
+        # GETS THE TABLE & VALIDATION DATA FOR THE MATRIX
+        if data_set == 'validation':
+            df = self.predval_table
+            x_val = ds.val_data
+        elif data_set == 'test':
+            df = self.predtest_table
+            x_val = ds.test_data
+
+        try:
+            df['round_pred']
+        except:
+            df = self.pred_round(df, threshold)
+
+        # INDICES FOR THE CONFUSION MATRIX
+        ind_tn = np.where( (df['round_pred'] == 0) & (df['gt'] == 0) )[0]
+        ind_fn = np.where( (df['round_pred'] == 0) & (df['gt'] == 1) )[0]
+        ind_tp = np.where( (df['round_pred'] == 1) & (df['gt'] == 1) )[0]
+        ind_fp = np.where( (df['round_pred'] == 1) & (df['gt'] == 0) )[0]
+
+        order = [ind_tn, ind_fp, ind_fn, ind_tp]
+        titles = ['True Negatives', 'False Positives',
+                  'False Negatives', 'True Positives']
+        shifts = [-2, 0, 2]
+
+        fig, axes = plt.subplots(ncols=2, nrows=2, figsize=(10,8))
+
+        i = 0
+
+        for ax in axes.reshape(-1):
+            inds = order[i]
+            which = np.random.randint(0,len(inds),3)
+
+            for j in range(3):
+                ax = plot_lc(x_val, inds[which[j]], ax, colors[j*2+1],
+                             shifts[j])
+
+            ax.set_title(titles[i], fontsize=20)
+
+            if titles[i] == 'False Positives' or titles[i] == 'False Negatives':
+                ax.set_facecolor('lightgray')
+
+            i += 1
+
+        return fig
+
