3ec806c194a0439e53f792f543d08f5c4203fbd2
Adina Feinstein <adina.d.feinstein@gmail.com>
2020-02-21 15:56:13 -0600

    i shall include you

--- stella/__init__.py
+++ stella/__init__.py
@@ -3,7 +3,9 @@ PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 
 from .version import __version__
 from .neural_network            import *
-from .preprocessing_data        import *
-from .recovery                  import *
+from .preprocessing_flares      import *
+#from .preprocessing_transits    import *
+#from .recovery                  import *
 from .visualize                 import *
 from .metrics                   import *
+from .rotations                 import *
