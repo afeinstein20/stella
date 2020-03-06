27b58fabf5f8d728469b4dc311b01233105f5140
Adina Feinstein <adina.d.feinstein@gmail.com>
2020-03-06 14:18:37 -0600

    more reliable fluurs

--- stella/mark_flares.py
+++ stella/mark_flares.py
@@ -92,6 +92,66 @@ class FitFlares(object):
         return np.array(results)
 
 
+    def get_init_guesses(self, groupings, time, flux, err, prob, 
+                         maskregion, region):
+        """
+        Guesses at the initial t0 and amplitude based on 
+        probability groups.
+
+        Parameters
+        ----------
+        groupings : np.ndarray
+             Group of indices for a single flare event.
+        time : np.array
+        flux : np.array
+        err : np.array
+        prob : np.array
+
+        Returns
+        -------
+        tpeaks : np.ndarray
+             Array of tpeaks for each flare group.
+        amps : np.ndarray
+             Array of amplitudes at each tpeak.
+        """
+        tpeaks = np.array([])
+        ampls  = np.array([])
+        if len(groupings) > 0:
+            for g in groupings:
+                gmin, gmax = g[0], g[-1]
+                
+                if gmin-maskregion < 0:
+                    subreg = np.arange(0, gmax+maskregion, 1, dtype=int)
+                elif gmax+maskregion > len(time):
+                    subreg = np.arange(len(time)-maskregion, len(time), 1, dtype=int)
+                else:
+                    subreg = np.arange(gmin-maskregion, gmax+maskregion, 1, dtype=int)
+
+                # LOOKS AT REGION AROUND FLARE                                                                 
+                subt = time[subreg]
+                subf = flux[subreg]
+                sube = err[subreg]
+                subp = prob[subreg]
+                
+                doubcheck = np.where(subp>=self.threshold)[0]
+            
+                # FINDS HIGHEST "PROBABILITY" IN FLARE                                                         
+                if len(doubcheck) > 1:
+                    peak = np.argmax(subf[doubcheck])
+                    t0   = subt[doubcheck[peak]]
+                    amp  = subf[doubcheck[peak]]
+
+                else:
+                    t0  = subt[doubcheck]
+                    amp = subf[doubcheck]
+                
+                if amp > (np.nanmedian(subf) + np.nanstd(subf)):
+                    tpeaks  = np.append(tpeaks, t0)
+                    ampls   = np.append(ampls,  amp)
+
+        return tpeaks, ampls
+
+
     def identify_flare_peaks(self, threshold=0.5):
         """
         Finds where the predicted value is above the threshold
@@ -106,11 +166,13 @@ class FitFlares(object):
 
         Attributes
         ----------
+        treshold : float
         flare_table : astropy.table.Table
              A table of flare times, amplitudes, and equivalent
              durations. Equivalent duration given in units of days.
         """
-        
+        self.threshold = threshold
+
         def chiSquare(var, x, y, yerr, t0_ind):
             """ Chi-square fit for flare parameters. """
             amp, rise, decay = var
@@ -119,11 +181,7 @@ class FitFlares(object):
 
         table = Table(names=['Target_ID', 'tpeak', 'amp', 'dur',
                              'rise', 'fall', 'prob'])
-
-        # SETS VARIABLES FOR FITTING FLARE & SURROUNDING REGION
-        region = 50
-        maskregion = 2
-        kernel_size= 7
+        kernel_size = 7
 
         for i in tqdm(range(len(self.IDs)), desc='Finding & Fitting Flares'):
             time, flux = self.time[i], self.flux[i]
@@ -132,71 +190,64 @@ class FitFlares(object):
             where_prob_higher = np.where(prob >= threshold)[0]
             groupings = self.group_inds(where_prob_higher)
 
-            # GETS ALL THE TPEAKS
-            tpeaks = np.array([])
-            if len(groupings) > 0:
-                for g in groupings:
-                    gmin, gmax = g[0], g[-1]
-                    
-                    # LOOKS AT REGION AROUND FLARE
-                    subt = time[gmin-region:gmax+region]
-                    subf = flux[gmin-region:gmax+region]
-                    sube = err[ gmin-region:gmax+region]
-                    subp = prob[gmin-region:gmax+region]
-
-                    inflare = prob[gmin-maskregion:gmax+maskregion]
-
-                    # FINDS HIGHEST "PROBABILITY" IN FLARE
-                    if len(np.where(subp > threshold)[0]) > 1:
-                        fpeak = np.nanmax(subf[np.where(subp > threshold)[0]])
-                        t0 = np.where(subf==fpeak)[0]
-                        if len(t0) > 1:
-                            t0 = int(np.argmax(subf[t0]))
-                            t0 = np.where(subf==subf[t0])[0]
-                    else:
-                        t0 = np.argmax(inflare)
-                    
-                    tpeaks = np.append(tpeaks, subt[t0])
-            tpeaks = np.unique(tpeaks)
+            tpeaks, amps = self.get_init_guesses(groupings, time, flux,
+                                                 err, prob, 2, 50)
 
             # FITS PARAMETERS TO FLARE
-            for tp in tpeaks:
-                where = np.where( (time>=tp) & (time<=tp+0.01) )[0][0]
+            for tp, amp in zip(tpeaks,amps):
+                # CASES FOR HANDLING BIG FLARES
+                if amp > 2.0:
+                    region = 400
+                    maskregion = 300
+                else:
+                    region = 50
+                    maskregion = 2
+
+                where = np.where(time >= tp)[0][0]
                 subt = time[where-region:where+region]
                 subf = flux[where-region:where+region]
                 sube = err[ where-region:where+region]
-                subp = prob[where-region:where+region]
-                
+                subp = prob[where-region:where+region] 
                 amp_ind = int(len(subf)/2)
-                
+
                 mask = np.append(np.arange(0,len(subt)/2-maskregion,1,dtype=int),
-                                 np.arange(len(subt)/2+maskregion,len(subt),1,dtype=int))
+                                np.arange(len(subt)/2+maskregion,len(subt),1,dtype=int))
 
-                func = interp1d(subt[mask], medfilt(subf[mask], kernel_size=kernel_size))
+                if len(mask) < 30:
+                    if amp > 5 and tp < time[0]+3:
+                        pass
+                else:
+                    func = interp1d(subt[mask], medfilt(subf[mask], kernel_size=kernel_size))
 
-                # REMOVES LOCAL STELLAR VARIABILITY TO FIT FLARE
-                detrended = subf/func(subt)
-                std = np.nanstd(detrended[mask])
-                med = np.nanmedian(detrended[mask])
-
-                # MARKS FLARE AMPLITUDE AND POINTS BEFORE & AFTER
-                amp = detrended[amp_ind]
-                decay  = detrended[amp_ind+1]
-                growth = detrended[amp_ind-1]
-
-                if ( (amp > (med+1.5*std)) and (decay >= (med+std)) and
-                     ((growth-1) < (amp-1)*0.95) ):
-                    x = minimize(chiSquare, x0=[amp-np.nanmedian(detrended[mask]), 0.0001, 0.0005],
-                                 args=(subt, detrended, sube, amp_ind),
-                                 method='L-BFGS-B')
-
-                    if x.x[2] != 0.0005:
-                        fm, params = flare_lightcurve(subt, amp_ind, x.x[0], x.x[1], x.x[2])
+                    # REMOVES LOCAL STELLAR VARIABILITY TO FIT FLARE
+                    detrended = subf/func(subt) 
+                    std = np.nanstd(detrended[mask])
+                    med = np.nanmedian(detrended[mask])
+                    
+                    # MARKS FLARE AMPLITUDE AND POINTS BEFORE & AFTER
+                    amp1 = detrended[amp_ind] - med
+                    decay  = detrended[amp_ind+1]
+                    growth = detrended[amp_ind-1]
+                
+                    if amp > 1.5:
+                        decay_guess = 0.001
+                    else:
+                        decay_guess = 0.0005
                         
-                        params[1] += 1
-                        params[2] = (params[2] * u.min).value / 2
-                        params = np.append(params, subp[amp_ind])
-                        params = np.append(np.array([self.IDs[i]]), params)
-                        table.add_row(params)
+                    if ( (amp > (med+1.5*std)) and (decay >= (med+std)) and
+                         ((growth-1) < (amp-1)*0.95) ):
+                        x = minimize(chiSquare, x0=[amp1, 0.0001, decay_guess],
+                                     args=(subt, detrended, sube, amp_ind),
+                                     method='L-BFGS-B')
+                    
+                        fm, params = flare_lightcurve(subt, amp_ind, np.nanmedian([amp1,x.x[0]]), 
+                                                      x.x[1], x.x[2])
+                    
+                        if x.x[0] > 1.5 or (x.x[0]<1.5 and x.x[2]<0.4):
+                            params[1] += 1
+                            params[2] = (params[2] * u.min).value / 2
+                            params = np.append(params, subp[amp_ind])
+                            params = np.append(np.array([self.IDs[i]]), params)
+                            table.add_row(params)
 
         self.flare_table = table
