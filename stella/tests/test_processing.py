426cb8e44ada8831d1860ae9e265688f0e1a1ee1
Adina Feinstein <adina.d.feinstein@gmail.com>
2020-07-29 11:40:13 -0500

    wrong file

--- stella/tests/test_processing.py
+++ stella/tests/test_processing.py
@@ -6,6 +6,6 @@ pre = FlareDataSet(fn_dir='.',
 
 def test_processing():
     assert_almost_equal(pre.frac_balance, 0.7, decimal=1)
-    assert(pre.train_data.shape == (67, 200, 1))
+    assert(pre.train_data.shape == (62, 200, 1))
     assert(pre.val_data.shape == (8, 200, 1))
     assert(pre.test_data.shape == (8, 200, 1))
