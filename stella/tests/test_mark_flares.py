e7608558ca1c6df312026cdfc982ad76f27c0e91
Adina Feinstein <adina.d.feinstein@gmail.com>
2021-02-16 14:13:58 -0600

    update test

--- stella/tests/test_mark_flares.py
+++ stella/tests/test_mark_flares.py
@@ -1,30 +1,30 @@
 import numpy as np
 from stella import ConvNN
 from stella import FitFlares
-from lightkurve.search import search_lightcurvefile
+from lightkurve.search import search_lightcurve
 from numpy.testing import assert_almost_equal
 
-lk = search_lightcurvefile(target='tic62124646', mission='TESS')
-lk = lk[(lk.author=='SPOC') & (lk.exptime.value==120)]
+lk = search_lightcurve(target='tic62124646', mission='TESS',
+                       exptime=120, sector=13)
 lk = lk.download()
-lk = lk.remove_nans()
+lk = lk.remove_nans().normalize()
 modelname = 'ensemble_s0002_i0010_b0.73.h5'
 
 cnn = ConvNN(output_dir='.')
 
 def test_predictions():
     cnn.predict(modelname=modelname,
-                times=lk.time.value,
-                fluxes=lk.pdcsap_flux.value,
-                errs=lk.pdcsap_flux_err.value)
+                times=lk.time,
+                fluxes=lk.flux,
+                errs=lk.flux_err)
     high_flares = np.where(cnn.predictions[0]>0.99)[0]
     assert(len(high_flares) == 0)
 
 def find_flares():
     flares = FitFlares(id=[lk.targetid],
-                       time=[lk.time.value],
-                       flux=[lk.pdcsap_flux.value],
-                       flux_err=[lk.pdcsap_flux_err.value],
+                       time=[lk.time],
+                       flux=[lk.flux],
+                       flux_err=[lk.flux_err],
                        predictions=[cn.predictions[0]])
 
     flares.identify_flare_peaks()
