d8fcb88cd2dd24471e9e462e2a898c4687115b86
Adina Feinstein <adina.d.feinstein@gmail.com>
2021-02-16 14:01:56 -0600

    lightkurve 2.0.0

--- stella/tests/test_mark_flares.py
+++ stella/tests/test_mark_flares.py
@@ -5,7 +5,8 @@ from lightkurve.search import search_lightcurvefile
 from numpy.testing import assert_almost_equal
 
 lk = search_lightcurvefile(target='tic62124646', mission='TESS')
-lk = lk.download().PDCSAP_FLUX
+lk = lk[(lk.author=='SPOC') & (lk.exptime.value==120)]
+lk = lk.download()
 lk = lk.remove_nans()
 modelname = 'ensemble_s0002_i0010_b0.73.h5'
 
@@ -13,17 +14,17 @@ cnn = ConvNN(output_dir='.')
 
 def test_predictions():
     cnn.predict(modelname=modelname,
-                times=lk.time,
-                fluxes=lk.flux,
-                errs=lk.flux_err)
+                times=lk.time.value,
+                fluxes=lk.pdcsap_flux.value,
+                errs=lk.pdcsap_flux_err.value)
     high_flares = np.where(cnn.predictions[0]>0.99)[0]
     assert(len(high_flares) == 0)
 
 def find_flares():
     flares = FitFlares(id=[lk.targetid],
-                       time=[lk.time],
-                       flux=[lk.flux],
-                       flux_err=[lk.flux_err],
+                       time=[lk.time.value],
+                       flux=[lk.pdcsap_flux.value],
+                       flux_err=[lk.pdcsap_flux_err.value],
                        predictions=[cn.predictions[0]])
 
     flares.identify_flare_peaks()
